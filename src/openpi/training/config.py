9db09f9e2abb4ab7c20aa781a54579c4ad1a1a13
John Doe <github@irislab.dooray.com>
2025-03-15 12:38:57 +0000

    fix: update train config

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -682,14 +682,17 @@ _CONFIGS = [
     # Franka bimanual configs.
     TrainConfig(
         name="pi0_franka_bimanual",
-        model=pi0.Pi0Config(paligemma_variant="gemma_300m_lora", action_expert_variant="gemma_300m_lora"),
+        model=pi0.Pi0Config(paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"),
         data=LeRobotFrankaBimanualDataConfig(
             repo_id="lerobot/aloha_sim_transfer_cube_human",
             default_prompt="Transfer cube",
             use_delta_joint_actions=False,
         ),
         weight_loader=weight_loaders.CheckpointWeightLoader("s3://openpi-assets/checkpoints/pi0_base/params"),
-        num_train_steps=30_000,
+        num_train_steps=3_000,
+        freeze_filter=pi0.Pi0Config(
+            paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"
+        ).get_freeze_filter()
     ),
     #
     # Debugging configs.
